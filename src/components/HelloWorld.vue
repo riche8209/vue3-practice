34d5f7fc7c76f144583b0aca5b3504ff8ec67a83
riche8209 <riche8209@gmail.com>
2023-09-14 23:00:43 +0900

    20230914 practice

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,11 +13,32 @@
     <p>
       message :: {{ outputMsg('아자??') }}
     </p>
+    <button @click="countUp()">countUp</button>
+    <h1> count :: {{ count }}</h1>
+    <button @click="addArray()">addArray</button>
+    <h1> myArray :: {{ myArray }}</h1>
+    <ul v-for="item in myArray" v-bind:key="item"> 
+      <li> array :: {{ item }} </li>
+    </ul>
+    <button @click="count1Up()">count1Up</button>
+    <h1>
+      count1 :: {{ count1 }}
+    </h1>
+    <h1>obj data</h1>
+    <p>{{ data }}</p>
+    <p>{{ data.objString }}</p>
+    <p>{{ data.objInt }}</p>
+    <p>{{ data.objJson }}</p>
+    <p>{{ data.objJson.a }}</p>
+    <p>{{ data.objBoolean }}</p>
+    <p>{{ data.objArray }}</p>
+    <p>{{ data.objArray[1] }}</p>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import { ref, reactive } from 'vue';
 
 export default {
   name: 'HelloWorld',
@@ -57,9 +78,51 @@ export default {
       console.log("axios 호출 res :: ", res);
     });
 
+    let count1 =0;
+    const count1Up = () => {
+      count1++;
+    };
+
+    const count = ref(0);
+    const countUp = () => {
+      count.value++;
+    };
+
+    let myArray = reactive([]);
+
+    const addArray = () => {
+      myArray.push('*');
+    };
+
+
+    const data = reactive({
+      'objString': 'string~~',
+      'objInt': 0,
+      'objArray': ['el1', 'el2', 'el3'],
+      'objJson': { 'a':'a',
+                   'b': 'b'},
+      'objBoolean': false
+    });
+
+    data.objString = 'change String';
+    data.objBoolean = !data.objBoolean;
+    data.objArray.pop();
+
+    data.objArray.push('push~');
+    data.objJson.c = 'c';
+
+    data.objJson['abcabc'] = 'abc?';
+
     return {
       greeting,
       outputMsg,
+      count,
+      countUp,
+      myArray,
+      addArray,
+      count1,
+      count1Up,
+      data,
     };
 
   }
